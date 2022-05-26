28ffd08bb1849909d740f5e628aec5bf7ac422b6
rakapramu <rkapramu25@gmail.com>
2022-05-26 18:55:08 +0700

    Final Project

---
+++ main.py
@@ -0,0 +1,15 @@
+import kode
+
+while True:
+    text = input('Masukkan > ')
+    if text.strip() == "":
+        continue
+    result, error = kode.run('<stdin>', text)
+
+    if error:
+        print(error.as_string())
+    elif result:
+        if len(result.elements) == 1:
+            print(repr(result.elements[0]))
+        else:
+            print(repr(result))
